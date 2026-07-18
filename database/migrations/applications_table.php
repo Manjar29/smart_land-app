eb5bcb274e547a795cd2dd61a2574c1fcfd2f9ba
Manjar <mdmanjarhossan@gmail.com>
2026-07-18 22:09:19 +0600

    mutation handle properly

--- database/migrations/applications_table.php
+++ database/migrations/applications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ApplicationsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -31,4 +31,4 @@ class ApplicationsTable extends Migration
     {
         Schema::dropIfExists('khajna_applications');
     }
-}
+};
